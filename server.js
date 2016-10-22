1ebd51dc3d1f67e646dd4786f4f561d4187d2cc8
purvi12 <prvpatel12@gmail.com>
2016-10-22 21:14:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('Article-one',function(req,res){
+app.get('aticle-one',function(req,res){
     res.send('Article-one is requested and will be served here');
 });
 
-app.get('Article two',function(req,res){
+app.get('aticle two',function(req,res){
 res.send('Article-two is requested and will be served here');
 });
 
-app.get('Article-three',function(req,res){
+app.get('aticle-three',function(req,res){
 res.send('Article-three is requested and will be served here');
 });
 app.get('/ui/style.css', function (req, res) {
